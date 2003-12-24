6ca9608a2e8a7487699dbb71a856b0725da462eb
khalid302 <khalid302>
2003-12-24 23:54:32 +0000

    * Implemented expr_free

--- main/src/qp.c
+++ main/src/qp.c
@@ -1,5 +1,17 @@
+/*                                                                *
+ * -------------------------------------------------------------- *
+ * The OpenXMLD                                                   *
+ * -------------------------------------------------------------- *
+ * This source file is subject to the GNU General Public licence, *
+ * which can be obtained through the world-wide-web at:           *
+ *                                                                *
+ *  http://www.gnu.org/copyleft/gpl.html                          *
+ * -------------------------------------------------------------- *
+ * Authors: Khalid Al-Kary (khalid_kary@hotmail.com)              *
+ * -------------------------------------------------------------- * 
+ */
+ 
 #include <iostream.h>
-#include "errors.h"
 #include "xmld_mempool.h"
 #include "xmld_types.h"
 #include "qp.h"
@@ -8,7 +20,7 @@ void qp_handle(void *conn) {
  struct XMLDWork *work=(struct XMLDWork *) malloc(sizeof(struct XMLDWork));
  /* FIXME the number of segments must come from config */
  struct XMLDMemPool *expr_pool=XMLDMemPool_create(sizeof(struct expr), 20, expr_free);
- struct XMLDMemPool *cond_pool=XMLDMemPool_create(sizeof(struct cond), 10, cond_free);
+ struct XMLDMemPool *cond_pool=XMLDMemPool_create(sizeof(struct cond), 10, NULL);
  work->conn=(struct XMLDConnection *) conn;
  work->req=(struct XMLDRequest *) malloc(sizeof(struct XMLDRequest));
  yyin=fopen(work->conn->fd, "r");
@@ -19,9 +31,4 @@ void qp_handle(void *conn) {
   return;
  }
  /* process req */ 
-}
-
-void expr_free(void *segment) {
-}
-void cond_free(void *segment) {
 }
--- main/src/qp.h
+++ main/src/qp.h
@@ -1,8 +1,19 @@
+/*                                                                *
+ * -------------------------------------------------------------- *
+ * The OpenXMLD                                                   *
+ * -------------------------------------------------------------- *
+ * This source file is subject to the GNU General Public licence, *
+ * which can be obtained through the world-wide-web at:           *
+ *                                                                *
+ *  http://www.gnu.org/copyleft/gpl.html                          *
+ * -------------------------------------------------------------- *
+ * Authors: Khalid Al-Kary (khalid_kary@hotmail.com)              *
+ * -------------------------------------------------------------- * 
+ */
+ 
 #ifndef HAVE_QP_H
 #define HAVE_QP_H
 
 void qp_handle(void *conn);
-void expr_free(void*);
-void cond_free(void*);
 
 #endif /* HAVE_QP_H */
