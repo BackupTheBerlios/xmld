b22a800a1ea98069e161dfa886aac0272090f9e5
khalid302 <khalid302>
2003-12-26 16:39:19 +0000

    * Added the licence note. * Removed an unneeded include.

--- main/src/fmanager.c
+++ main/src/fmanager.c
@@ -1,4 +1,16 @@
-#include <iostream.h>
+/*                                                                *
+ * -------------------------------------------------------------- *
+ * The OpenXMLD                                                   *
+ * -------------------------------------------------------------- *
+ * This source file is subject to the GNU General Public licence, *
+ * which can be obtained through the world-wide-web at:           *
+ *                                                                *
+ *  http://www.gnu.org/copyleft/gpl.html                          *
+ * -------------------------------------------------------------- *
+ * Authors: Khalid Al-Kary (khalid_kary@hotmail.com)              *
+ * -------------------------------------------------------------- * 
+ */
+
 #include <stdlib.h>
 #include <string.h>
 #include "errors.h"
@@ -7,14 +19,14 @@
 
 /* FIXME: file config should be checked to decide the engine to be used.
  * Currently we use only one engine defined in engine_xmld.h
- * a variable of type struct XMLDEngine called `engine' carries
+ * a variable of type struct XMLDEngine called `engine_xmld' carries
  * this only one engine 
  */
 
 xmld_status_t fmanager_handle(struct XMLDWork *work) {
  work->res=(struct XMLDResource *) malloc(sizeof(struct XMLDResource));
  work->res->engine=&engine_xmld;
- work->res->data_source=(*(work->res->engine->init)) (work); /* must descide mode according to XMLDRequest->type */
+ work->res->data_source=(*(work->res->engine->init)) (work);
  if (work->res->data_source==0) {
   return XMLD_FAILURE;
  }
