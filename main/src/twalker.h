ac119984ca9aa4ff7de8a6aea83da312482feb54
khalid302 <khalid302>
2003-12-27 21:47:05 +0000

    * Added the licence note.

--- main/src/twalker.h
+++ main/src/twalker.h
@@ -1,3 +1,16 @@
+/*                                                                *
+ * -------------------------------------------------------------- *
+ * The OpenXMLD                                                   *
+ * -------------------------------------------------------------- *
+ * This source file is subject to the GNU General Public licence, *
+ * which can be obtained through the world-wide-web at:           *
+ *                                                                *
+ *  http://www.gnu.org/copyleft/gpl.html                          *
+ * -------------------------------------------------------------- *
+ * Authors: Khalid Al-Kary (khalid_kary@hotmail.com)              *
+ * -------------------------------------------------------------- * 
+ */
+
 #ifndef HAVE_TWALKER_H
 #define HAVE_TWALKER_H
 
