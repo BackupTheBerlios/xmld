304cea832223465ee8118aa39dc1d9aacd6ca186
khalid302 <khalid302>
2004-01-03 22:44:09 +0000

    * Splitted xmld_types.h to make the OpenXMLD have an object-oriented foundation.

---
+++ main/src/xmld_aggr_table.h
@@ -0,0 +1,30 @@
+/*                                                                *
+ * -------------------------------------------------------------- *
+ * The OpenXMLD                                                   *
+ * -------------------------------------------------------------- *
+ * This source file is subject to the GNU General Public licence, *
+ * which can be obtained through the world-wide-web at:           *
+ *                                                                *
+ *  http://www.gnu.org/copyleft/gpl.html                          *
+ * -------------------------------------------------------------- *
+ * Authors: Khalid Al-Kary (khalid_kary@hotmail.com)              *
+ * -------------------------------------------------------------- * 
+ */
+
+#ifndef HAVE_XMLD_AGGR_TABLE
+#define HAVE_XMLD_AGGR_TABLE
+
+/* represents an expression -> set of columns assocsiation */
+struct XMLDAggrTable {
+ XMLDExpr *aggr;
+ XMLDRow *cols;
+};
+
+typedef struct XMLDAggrTable XMLDAggrTable;
+
+void XMLDAggrTable_free_content(XMLDAggrTable *);
+void XMLDAggrTable_free(XMLDAggrTable *);
+void XMLDAggrTable_add_col(XMLDAggrTable *, XMLDCol *);
+void XMLDAggrTable_fill(XMLDAggrTable *, char *);
+
+#endif /* HAVE_XMLD_AGGR_TABLE */
