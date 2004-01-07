45de3cc53bce6551221c3d910e75a1292271916a
khalid302 <khalid302>
2004-01-07 08:48:52 +0000

    * removed struct xmld_part.

--- main/src/init.h
+++ main/src/init.h
@@ -14,13 +14,6 @@
 #ifndef HAVE_INIT_H
 #define HAVE_INIT_H
 
-/* A structure representing a part that needs initialization */
-struct xmld_part {
- xmld_status_t (*init_func) (void); /* initialization function */
- xmld_status_t (*shutdown_func) (void); /* shutdown function */
- int ok; /* whether initilization went fine */
-};
+void init_shutdown_parts(void)
 
-void init_create_part(struct xmld_part*, xmld_status_t (*) (void), xmld_status_t (*) (void));
-void init_shutdown_parts(void);
 #endif /* HAVE_INIT_H */
