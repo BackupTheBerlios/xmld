52fdf2b4021f0a84947aae5795477cd5277b5971
khalid302 <khalid302>
2004-01-09 11:53:38 +0000

    * XMLDCol_fill definition. * XMLDCol_add_to_list returns void* to the item that was added.

--- main/src/xmld_col.h
+++ main/src/xmld_col.h
@@ -21,11 +21,12 @@ struct XMLDCol {
 typedef struct XMLDCol XMLDCol;
 
 XMLDCol *XMLDCol_create(char *, short); /* short is whether to copy the char **/
+void XMLDCol_fill(XMLDCol *, char *, short);
 void XMLDCol_free(XMLDCol *);
 
 /* List functions */
 
 XMLDList *XMLDCol_create_list(void);
-void XMLDCol_add_to_list(XMLDList *, char *, short);
+void *XMLDCol_add_to_list(XMLDList *, char *, short);
 
 #endif /* HAVE_XMLD_COL_H */
