8a492b59abe5706f44d228361dfa6e2e7eab7d4e
khalid302 <khalid302>
2004-01-24 23:44:01 +0000

    * changed definitions to fit the new XMLDList editions.

--- main/src/xmld_aggr_table.h
+++ main/src/xmld_aggr_table.h
@@ -14,23 +14,24 @@
 #ifndef HAVE_XMLD_AGGR_TABLE_h
 #define HAVE_XMLD_AGGR_TABLE_h
 
-/* represents an expression -> set of columns assocsiation */
+/* represents an (aggregate expression -> set of columns) assocsiation */
 struct XMLDAggrTable {
  XMLDExpr *aggr;
- XMLDRow *cols;
+ XMLDList *col_ptrs; /* list of "XMLDCol*" */
 };
 
 typedef struct XMLDAggrTable XMLDAggrTable;
 
-XMLDAggrTable *XMLDAggrTable_create(XMLDExpr *, XMLDRow *);
+XMLDAggrTable *XMLDAggrTable_create(XMLDExpr *, XMLDList *);
 void XMLDAggrTable_free(XMLDAggrTable *);
+void XMLDAggrTable_free_content(void *);
 void XMLDAggrTable_add_col(XMLDAggrTable *, XMLDCol *);
 void XMLDAggrTable_fill(XMLDAggrTable *, char *);
 
 /* List functions */
 
 XMLDList *XMLDAggrTable_create_list(void);
-void XMLDAggrTable_add_to_list(XMLDList *, XMLDExpr *, XMLDRow *);
+XMLDAggrTable *XMLDAggrTable_add_to_list(XMLDList *, XMLDExpr *, XMLDList *);
 XMLDAggrTable *XMLDAggrTable_search_list_by_expr(XMLDList *, XMLDExpr *);
 
 #endif /* HAVE_XMLD_AGGR_TABLE_h */
