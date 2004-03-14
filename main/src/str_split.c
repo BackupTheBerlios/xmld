3a8b95f823f0a721717ca7e8959e97131ade5648
khalid302 <khalid302>
2004-03-14 22:14:45 +0000

    * Support for splitting a string into tokens using a given character.

---
+++ main/src/str_split.c
@@ -0,0 +1,28 @@
+/*                                                                *
+ * -------------------------------------------------------------- *
+ * The OpenXMLD                                                   *
+ * -------------------------------------------------------------- *
+ * This source file is subject to the GNU General Public licence, *
+ * which can be obtained through the world-wide-web at:           *
+ *                                                                *
+ *  http://www.gnu.org/copyleft/gpl.html                          *
+ * -------------------------------------------------------------- *
+ * Authors: Khalid Al-Kary (khalid_kary@hotmail.com)              *
+ * -------------------------------------------------------------- * 
+ */
+
+#include <stdlib.h>
+#include "str_split.h"
+
+/*
+ * : Returns an array of strings extracted
+ * from the given string by breaking it into
+ * pieces separated by the given delimeter.
+ * str: The string from which to extract tokens.
+ * delim: The delimeter to use in breaking the given
+ * string.
+ * returns: An array of the extracted tokens.
+ */ 
+char **str_split(char *str, char delim) {
+ return (char **) NULL;
+}
---
+++ main/src/str_split.h
@@ -0,0 +1,20 @@
+/*                                                                *
+ * -------------------------------------------------------------- *
+ * The OpenXMLD                                                   *
+ * -------------------------------------------------------------- *
+ * This source file is subject to the GNU General Public licence, *
+ * which can be obtained through the world-wide-web at:           *
+ *                                                                *
+ *  http://www.gnu.org/copyleft/gpl.html                          *
+ * -------------------------------------------------------------- *
+ * Authors: Khalid Al-Kary (khalid_kary@hotmail.com)              *
+ * -------------------------------------------------------------- * 
+ */
+
+#ifndef HAVE_STR_SPLIT_H
+#define HAVE_STR_SPLIT_H
+
+char **str_split(char *, char);
+
+#endif /* HAVE_STR_SPLIT_H */
+
