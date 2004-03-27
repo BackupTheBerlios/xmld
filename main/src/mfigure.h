abe57b3efe1e780cc12b80350bf4e0e34c9abf20
khalid302 <khalid302>
2004-03-27 18:26:10 +0000

    no message

---
+++ main/src/mfigure.h
@@ -0,0 +1,19 @@
+/*                                                                *
+ * -------------------------------------------------------------- *
+ * The OpenXMLD                                                   *
+ * -------------------------------------------------------------- *
+ * This source file is subject to the GNU General Public licence, *
+ * which can be obtained through the world-wide-web at:           *
+ *                                                                *
+ *  http://www.gnu.org/copyleft/gpl.html                          *
+ * -------------------------------------------------------------- *
+ * Authors: Khalid Al-Kary (khalid_kary@hotmail.com)              *
+ * -------------------------------------------------------------- * 
+ */
+ 
+#ifndef HAVE_MFIGURE_H
+#define HAVE_MFIGURE_H
+
+char *mfigure_get_mime(char *);
+
+#endif /* HAVE_MFIGURE_H */
