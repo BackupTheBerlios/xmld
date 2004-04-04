6adf6775c42d47173c3cd5af335197292f9a9531
khalid302 <khalid302>
2004-04-04 05:17:19 +0000

    * Inclusion of func_list_init/shutdown

--- main/src/xmld.c
+++ main/src/xmld.c
@@ -10,10 +10,12 @@
  * Authors: Khalid Al-Kary (khalid_kary@hotmail.com)              *
  * -------------------------------------------------------------- * 
  */
- 
+
+#include <stdio.h>
 #include "cfg.h"
 
 #ifdef USE_PTASKER
+ #include <sys/types.h>
  #include "ptasker/ptasker.h"
  #define MULTI_PROC_MTASKER
  #undef MULTI_THREAD_MTASKER
@@ -23,23 +25,25 @@
 #include "sosel.h"
 #include "init.h"
 #include "engine_list.h"
+#include "func_list.h"
 
-struct xmld_part parts[4];
+struct xmld_part parts[6];
 short status;
 int i;
 
 int main() {
- xmld_create_part(&parts[0], cfg_init, cfg_shutdown);
- xmld_create_part(&parts[1], mtasker_init, mtasker_shutdown);
- xmld_create_part(&parts[2], somanager_init, somanager_shutdown);
- xmld_create_part(&parts[3], sosel_init, sosel_shutdown);
- xmld_create_part(&parts[4], engine_list_init, engine_list_shutdown);
+ init_create_part(&parts[0], cfg_init, cfg_shutdown);
+ init_create_part(&parts[1], mtasker_init, mtasker_shutdown);
+ init_create_part(&parts[2], somanager_init, somanager_shutdown);
+ init_create_part(&parts[3], sosel_init, sosel_shutdown);
+ init_create_part(&parts[4], engine_list_init, engine_list_shutdown);
+ init_create_part(&parts[5], func_list_init, func_list_shutdown);
 
  for (i=0;i<4;i++) {
   status=(*(parts[i].init_func))();
   if (status!=0) {
    perror("Initializer");
-   xmld_shutdown_parts();
+   init_shutdown_parts();
   }
   else {
    parts[i].ok=1;
