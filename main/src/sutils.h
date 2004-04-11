3029a17c52af8e7c14361968a51d4aa8a2981a1d
khalid302 <khalid302>
2004-04-11 08:35:42 +0000

    * Definition of  itoa.

--- main/src/sutils.h
+++ main/src/sutils.h
@@ -18,6 +18,7 @@ char **str_split(char *, char);
 char *str_prepend(char *, char *);
 short str_like(char *, char *);
 short str_between(char *, char *, char *);
+char *itoa(int);
 
 #endif /* __SUTILS_H */
 
