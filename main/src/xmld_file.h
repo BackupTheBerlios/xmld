07ddbf31c92f3584186c3b1f1128baa16e6e53cb
khalid302 <khalid302>
2004-08-09 22:49:04 +0000

    No Message

--- main/src/xmld_file.h
+++ main/src/xmld_file.h
@@ -17,7 +17,7 @@
 struct XMLDFile {
  char *name;  /* The name of the file       */
  void *data;  /* File's data representation */
- void *store; /* Additional storage field   */
+ XMLDFile *store; /* Additional storage field   */
  XMLDEngine *engine; /* The engine to be used for this file */
  int level; /* The current level in the file */
  fpos_t element_pos; /* The position of the beginning of the current element in the file */
