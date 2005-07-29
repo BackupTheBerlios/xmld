d16892ec9d734292bf7f91a40c9a6abd0f501a07
khalid302 <khalid302>
2005-07-29 15:02:29 +0000

    *** empty log message ***

--- main/src/xmld_engine.h
+++ main/src/xmld_engine.h
@@ -30,8 +30,14 @@
 #define ENGINE_CAP_NODE_ABSOLUTE_VALUE 4
 #define ENGINE_CAP_LABEL_ITERATION 6
 #define ENGINE_CAP_NODE_MODIFY 8
+#define ENGINE_CAP_NODE_CREATE 10
+#define ENGINE_CAP_NODE_ALTER 12
+#define ENGINE_CAP_NODE_REMOVE 14
 
 struct XMLDEngine {
+ 
+ /* Basic functions */
+
  int engine_cap;
  void (*init) (void);
  XMLDBool (*is_valid_mime) (char *);
@@ -39,6 +45,9 @@ struct XMLDEngine {
  void (*cleanup) (XMLDFile *);
  void (*destroy) (void);
  int (*walk) (XMLDFile *);
+
+ /* Data source reading API */
+ 
  char *(*get_labeled_node_value_type) (XMLDFile *, char *);
  int (*get_labeled_node_value_length) (XMLDFile *, char *);
  char *(*get_labeled_node_value) (XMLDFile *, char *);
@@ -56,11 +65,39 @@ struct XMLDEngine {
  int (*get_current_labeled_value_length) (XMLDFile *);
  char *(*get_current_labeled_value) (XMLDFile *);
 
+ /* Iterative labeled node value modification API */
+ 
+ XMLDBool (*set_current_label) (XMLDFile *, char *);
+ /* Require alteration capability bit for the next two functions */
+ XMLDBool (*set_current_labeled_value_type) (XMLDFile *, char *);
+ XMLDBool (*set_current_labeled_value_length) (XMLDFile *, int);
+ XMLDBool (*set_current_labeled_value) (XMLDFile *, char *);
+ 
  /* Data source modification API */
 
  XMLDBool (*set_labeled_node_value) (XMLDFile *, char *, char *);
  XMLDBool (*set_absolute_node_value) (XMLDFile *, char *);
  XMLDBool (*set_node_name) (XMLDFile *, char *); 
+
+ /* Data source creation API */
+
+ XMLDFile *(*create_source) (void);
+ XMLDBool (*create_node) (XMLDFile *, char *);
+ XMLDBool (*create_labeled_node_value) (XMLDFile *, char *, char *);
+
+ /* Data source alteration API */
+
+ XMLDBool (*set_labeled_node_value_type) (XMLDFile *, char *);
+ XMLDBool (*set_labeled_node_value_length) (XMLDFile *, char *);
+ XMLDBool (*set_absolute_node_value_type) (XMLDFile *);
+ XMLDBool (*set_absoulte_node_value_length) (XMLDFile *);
+ 
+ /* Data source removal API */
+
+ XMLDBool (*remove_source) (XMLDFile *);
+ XMLDBool (*remove_node) (XMLDFile *);
+ XMLDBool (*remove_labeled_node_value) (XMLDFile *, char *);
+ XMLDBool (*remove_absolute_node_value) (XMLDFile *);
 };
 
 #ifndef XMLDENGINE_TYPE_DEFINED
