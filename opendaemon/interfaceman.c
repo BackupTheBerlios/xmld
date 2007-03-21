bddc217edad4e855e295a83657f8162ec2c1bc63
khalid302 <khalid302>
2007-03-21 22:13:23 +0000

    * Fixing 0.8.0 blocking bugs

--- opendaemon/interfaceman.c
+++ opendaemon/interfaceman.c
@@ -1,6 +1,6 @@
 /*                                                                *
  * -------------------------------------------------------------- *
- * The OpenXMLD                                                   *
+ * OpenDaemon                                                     *
  * -------------------------------------------------------------- *
  * This source file is subject to the GNU General Public licence, *
  * which can be obtained through the world-wide-web at:           *
@@ -13,16 +13,33 @@
 
 #include <pwd.h>
 #include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/msg.h>
 #include <unistd.h>
 #include "includes.h"
 #include "process_pool.h"
 #include "opendaemon.h"
 #include "classes/interface.h"
+#include <errno.h>
+
+int code_len = 0;
+int conf_len = 0;
+int user_len = 0;
 
 Status interfaceman_init(void) {
+ key_t queue_key = ftok("opendaemon.h", 'X');
+ int queue_id = msgget(queue_key, IPC_CREAT);
+
+ struct msgbuf buf;
+
+ if (queue_id == -1) {
+  printf("\t* Interface manager: Unable to initialize a message queue for the interface manager.\n");
+  return FAILURE;
+ }
+
  CfgSection *interface_section;
  CfgDirective *interface_directive;
- CfgValue *interface_params[3];
+ CfgValue *interface_value;
  int i = 0;
 
  while ((interface_section = CfgSection_get_section(cfg_tree, "Interface", i)) != NULL) {
@@ -32,67 +49,117 @@ Status interfaceman_init(void) {
    continue;
   }
 
-  interface_params[0] = CfgDirective_get_value(interface_directive, 0);
-  if (interface_params[0] == NULL || interface_params[0]->type != CFG_STRING) {
+  interface_value = CfgDirective_get_value(interface_directive, 0);
+  if (interface_value == NULL || interface_value->type != CFG_STRING) {
    printf("\t* Interface manager: Skipping an interface with an undefined or malformed Code directive value.\n");
    continue;
   }
   
+  buf.mtype = MTYPE_CODE;
+  code_len = strlen((char *) interface_value->value);
+  strncpy(buf.mtext, (char *) interface_value->value, MAX_MSG_LEN);
+  buf.mtext[MAX_MSG_LEN - 1] = '\0';
+  msgsnd(queue_id, &buf, (code_len + 1), 0);
+  
   interface_directive = CfgSection_get_directive(interface_section, "Conf", 0);
   if (interface_directive != NULL) {
-   interface_params[1] = CfgDirective_get_value(interface_directive, 0);
-   if (interface_params[1]->type != CFG_STRING) {
-    interface_params[1] = NULL;
+   interface_value = CfgDirective_get_value(interface_directive, 0);
+   if (interface_value->type != CFG_STRING) {
+    interface_value = NULL;
    }
   }
   else {
-   interface_params[1] = NULL;
+   interface_value = NULL;
   }
+  
+  buf.mtype = MTYPE_CONF;
+  if (interface_value != NULL) {
+   conf_len = strlen((char *) interface_value->value);
+   strncpy(buf.mtext, (char *) interface_value->value, MAX_MSG_LEN);
+   buf.mtext[MAX_MSG_LEN - 1] = '\0';
+  }
+
+  msgsnd(queue_id, &buf, (conf_len + 1), 0);
   
   interface_directive = CfgSection_get_directive(interface_section, "User", 0);
   if (interface_directive != NULL) {
-   interface_params[2] = CfgDirective_get_value(interface_directive, 0);
-   if (interface_params[2]->type != CFG_STRING) {
-    interface_params[2] = NULL;
+   interface_value = CfgDirective_get_value(interface_directive, 0);
+   if (interface_value->type != CFG_STRING) {
+    interface_value = NULL;
    }
   }
   else {
-   interface_params[2] = NULL;
+   interface_value = NULL;
   }
   
-  process_pool_handle(main_proc_pool, _launch, (void *) &interface_params);
+  if (interface_value != NULL) {
+   buf.mtype = MTYPE_USER;
+   user_len = strlen((char *) interface_value->value);
+   strncpy(buf.mtext, (char *) interface_value->value, MAX_MSG_LEN);
+   buf.mtext[MAX_MSG_LEN - 1] = '\0';
+  }
+
+  msgsnd(queue_id, &buf, (user_len + 1), 0);
+  
+  process_pool_handle(main_proc_pool, _launch, (void *) NULL);
   i++;
  }
  return SUCCESS;
 }
 
-void _launch(void *params) {
- CfgValue **cfg_params = (CfgValue **) params;
- if (*(cfg_params + 2) != NULL) {
-  struct passwd *user_id_struct = getpwnam((*(cfg_params + 2))->value);
+void _launch(void *null) {
+ char *code;
+ char *conf = NULL;
+ char *user = NULL;
+
+ key_t queue_key = ftok("opendaemon.h", 'X');
+ int queue_id = msgget(queue_key, 0);
+ struct msgbuf buf;
+
+ buf.mtype = MTYPE_CODE;
+ code_len = msgrcv(queue_id, &buf, MAX_MSG_LEN * sizeof(char), MTYPE_CODE, 0);
+ code = (char *) malloc(code_len);
+ strcpy(code, buf.mtext);
+sleep(50); 
+ buf.mtype = MTYPE_CONF;
+ conf_len = msgrcv(queue_id, &buf, MAX_MSG_LEN * sizeof(char), MTYPE_CONF, 0);
+ if (conf_len != -1) {
+  conf = (char *) malloc(conf_len + 1);
+  strcpy(conf, buf.mtext);
+ }
+
+ buf.mtype = MTYPE_USER;
+ user_len = msgrcv(queue_id, &buf, MAX_MSG_LEN * sizeof(char), MTYPE_USER, 0);
+ if (user_len != -1) {
+  user = (char *) malloc(user_len + 1);
+  strcpy(user, buf.mtext);
+ }
+
+ if (user != NULL) {
+  struct passwd *user_id_struct = getpwnam(user);
   if (setuid(user_id_struct->pw_uid) != 0) {
-   printf("\t* Interface manager: Unable to set the user to %s for the process of an interface loading the code %s\n", (char *) (*(cfg_params + 2))->value, (char *) (*(cfg_params))->value);
+   printf("\t* Interface manager: Unable to set the user to %s for the process of an interface loading the code %s\n", user, code);
    return;
   }
  }
 
- Module *mod = modman_load_module((*cfg_params)->value, MODULE_INTERFACE_MODULE);
+ Module *mod = modman_load_module(code, MODULE_INTERFACE_MODULE);
  if (mod == NULL) {
-  printf("\t* Interface manager: Error loading interface module %s\n", (char *) (*(cfg_params))->value);
+  printf("\t* Interface manager: Error loading interface module %s\n", code);
   return;
  }
 
- Interface *if_inst = (Interface *) modman_get_module_instance(mod, (char *) (*(cfg_params + 1))->value);
+ Interface *if_inst = (Interface *) modman_get_module_instance(mod, conf);
  Error *msg;
 
  if ((*(if_inst->init)) (if_inst) == FAILURE) {
   msg = (*if_inst->get_error) (if_inst);
-  printf("\t* Interface manager: Error initializing interface module %s: %s\n", (char *) (*(cfg_params))->value, msg->message);
+  printf("\t* Interface manager: Error initializing interface module %s: %s\n", code, msg->message);
   return;
  }
  else {
   msg = (*if_inst->get_error) (if_inst);
-  printf("\t* %s: %s", (char *) (*(cfg_params))->value, msg->message);
+  printf("\t* %s: %s", code, msg->message);
   (*(if_inst->main)) (if_inst); /* Let her do her job */
   (*(if_inst->destroy)) (if_inst); /* Kill her! */
  }
--- opendaemon/interfaceman.h
+++ opendaemon/interfaceman.h
@@ -14,6 +14,16 @@
 #ifndef __INTERFACEMAN_H
 #define __INTERFACEMAN_H
 
+#define MTYPE_CODE 1
+#define MTYPE_CONF 2
+#define MTYPE_USER 3
+#define MAX_MSG_LEN 50
+
+struct msgbuf {
+ long mtype;
+ char mtext[50];
+};
+
 Status interfaceman_init(void);
 void _launch(void *);
 
